710a4d69ef469b46f447739be37fd62371de8398
Max DePriest <maxd924@gmail.com>
2015-06-22 13:35:55 -0500

    FK working and form loading correctly. Working on output next

--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root :to => 'users#new'
+  root 'users#new'
   resources :user_sessions
   resources :users
 
