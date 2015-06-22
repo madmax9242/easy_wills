1a911d18d466c05bb3f3815ac5dbd43be2ff3d7e
Max DePriest <maxd924@gmail.com>
2015-06-22 09:39:59 -0500

    Storing form before I start deleting shtuff

--- app/controllers/forms_controller.rb
+++ app/controllers/forms_controller.rb
@@ -5,11 +5,12 @@ class FormsController < ApplicationController
 
 	def create
 		@form = Form.new
+		@form.save
 
 		respond_to do |format|
 			if @form.save
 				format.html { redirect_to @form, notice: 'Form was successfully created.' }
-				format.json { render :show, status: :created, location: @user }
+				format.json { render :show, status: :created, location: @form }
 			else
 				format.html { render :new }
 				format.json { render json: @form.errors, status: :unprocessable_entity }
--- app/models/form.rb
+++ app/models/form.rb
@@ -1,3 +1,4 @@
 class Form < ActiveRecord::Base
+	field :gh_ami_cc_only, gh_ami_cc_only: String
 	belongs_to :user
 end
