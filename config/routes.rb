a0e5b2bf63cf22673132c2da77ebb34417eafb4b
Max DePriest <maxd924@gmail.com>
2015-07-15 16:23:14 -0500

    adding routes and form helpers

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,10 @@ Rails.application.routes.draw do
 
   resources :users
   resources :forms
+  resources :first_scenario
+  resources :second_scenario
+  resources :third_scenario
+  resources :fourth_scenario
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
