4f475e0c13e23d74d557f8c4f152daaff8a3cca7
Max DePriest <maxd924@gmail.com>
2015-07-27 14:56:01 -0500

    added progress bar

--- app/assets/javascripts/form.js
+++ app/assets/javascripts/form.js
@@ -13,17 +13,23 @@ function form_change() {
 		event.preventDefault;
 		$('#first').addClass("hidden");
 		$('#second').toggleClass('hidden');
+		$('.progress-bar').text("25%");
+		$('.progress-bar').attr("style", "width:25%");
 	});
 
 	$('#to_3').on('click', function(event) {
 		event.preventDefault;
 		$('#second').addClass("hidden");
 		$('#third').removeClass('hidden');
+		$('.progress-bar').text("50%");
+		$('.progress-bar').attr("style", "width:50%");
 	});
 
 	$('#to_4').on('click', function(event) {
 		event.preventDefault;
 		$('#third').addClass("hidden");
 		$('#fourth').removeClass('hidden');
+		$('.progress-bar').text("75%");
+		$('.progress-bar').attr("style", "width:75%");
 	});
 }
