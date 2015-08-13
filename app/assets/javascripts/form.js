b0957a00d910eb6fc65795d1e7a474c78fbe8205
Max DePriest <maxd924@gmail.com>
2015-08-12 19:24:43 -0500

    fiddling around with creating a new User

--- app/assets/javascripts/form.js
+++ app/assets/javascripts/form.js
@@ -7,6 +7,7 @@ function initialize() {
 	form_change();
 	intro_events();
 	general_events();
+	new_user_events();
 
 
 };
@@ -22,7 +23,6 @@ function form_change() {
 		$('.progress-bar').attr("style", "width:25%");
 	});
 
-
 	$('#to_3').on('click', function(event) {
 		event.preventDefault;
 		$('#second').addClass("hidden");
@@ -30,7 +30,6 @@ function form_change() {
 		$('.progress-bar').text("50%");
 		$('.progress-bar').attr("style", "width:50%");
 	});
-
 
 	$('#to_4').on('click', function(event) {
 		event.preventDefault;
@@ -40,7 +39,32 @@ function form_change() {
 		$('.progress-bar').attr("style", "width:75%");
 	});
 
+	$('#back_to_1').on('click', function(event) {
+		event.preventDefault;
+		$('#second').addClass("hidden");
+		$('#first').removeClass("hidden");
+		$('.progress-bar').text("0%");
+		$('.progress-bar').attr("style", "width:0%");
+	});
+
+	$('#back_to_2').on('click', function(event) {
+		event.preventDefault;
+		$('#third').addClass("hidden");
+		$('#second').removeClass("hidden");
+		$('.progress-bar').text("25%");
+		$('.progress-bar').attr("style", "width:25%");
+	});
+
+	$('#back_to_3').on('click', function(event) {
+		event.preventDefault;
+		$('#fourth').addClass("hidden");
+		$('#third').removeClass("hidden");
+		$('.progress-bar').text("50%");
+		$('.progress-bar').attr("style", "width:50%");
+	});
+
 }
+
 
 
 function intro_events() {
@@ -51,11 +75,25 @@ function intro_events() {
 
 }
 
+function new_user_events() {
+	$('#to_QL').on('click', function(event) {
+		event.preventDefault;
+		$('#basic_info').addClass("hidden");
+		$('#quality').removeClass("hidden");
+	});
+
+	$('#back_to_basics').on('click', function(event) {
+		event.preventDefault;
+		$('#quality').addClass("hidden");
+		$('#basic_info').removeClass("hidden");
+	})
+}
+
 function general_events() {
-	$('.next-button, .back-button').on('mouseover', function() {
+	$('.next-button, .back-button, button').on('mouseover', function() {
 		$(this).addClass('underline');
 	});
-	$('.next-button, .back-button').on('mouseout', function() {
+	$('.next-button, .back-button, button').on('mouseout', function() {
 		$(this).removeClass('underline');
 	});
 }
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -27,7 +27,7 @@ class UsersController < ApplicationController
     respond_to do |format|
       if @user.save
         auto_login(@user)
-        format.html { redirect_to :users, notice: 'User was successfully created.' }
+        format.html { redirect_to new_form_path, notice: 'User was successfully created.' }
         format.json { render :show, status: :created, location: @user }
       else
         format.html { render :new }
